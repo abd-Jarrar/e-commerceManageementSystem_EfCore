35ca8a05365a2f4c15e65235aef6447b7a10b81b
abood.jarrar <abd.a.jarrar@gmail.com>
2026-08-12 17:53:37 +0300

    create the configuration files

---
+++ Configuration/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using E_CommerceManagementSystemEfCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceManagementSystemEfCore.Configuration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

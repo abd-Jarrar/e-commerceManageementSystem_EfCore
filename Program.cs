cb38c504298423ca634f612769cedc395079e6df
abood.jarrar <abd.a.jarrar@gmail.com>
2026-08-14 21:27:14 +0300

    change the structure of the project

--- Program.cs
+++ Program.cs
@@ -1,49 +1,32 @@
-﻿using E_CommerceManagementSystemEfCore.Data;
-using E_CommerceManagementSystemEfCore.Entities;
+﻿using ECommerce.Business.Entities;
 using Microsoft.EntityFrameworkCore;
+using ECommerce.Database.Data;
+using ECommerce.Database.Interceptors;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 
-namespace E_CommerceManagementSystemEfCore
+namespace ECommerce.App
 {
     public class Program
     {
-        static bool IsExpensive(Product product)
-        {
-            return product.Price > 100;
-        }
         static void Main(string[] args)
         {
-            using(var context=new AppDbContext())
-            {
-                var products = context.Products.AsNoTracking();
-                
-                foreach (var product in products)
-                {
-                    Console.WriteLine($"{product.Name}, {product.Price}");
+            var configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json")
+           .Build();
 
-                }
-                var productWithoutPrice = context.Products.Select(x => new
-                {
-                    x.Id,
-                    x.Name
-                }).ToList();//top level projection
+            var connectionString = configuration
+                .GetSection("constr")
+                .Value;
 
-                foreach (var product in productWithoutPrice)
-                {
-                    Console.WriteLine($"{product.Id}, {product.Name}");
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(connectionString)
+                .AddInterceptors(new SoftDeleteInterceptor())
+                .Options;
 
-                }
+            using var context = new AppDbContext(options);
 
-                var Categories = context.Categories.Include(x => x.Products).AsNoTracking();//eager loading
-                context.Reviews.ExecuteUpdate(x => x.SetProperty(x => x.Rating , 8));//effecient update
-
-                var name = "abood";
-                var users = context.Users.
-                    FromSqlInterpolated($"select * from users where FullName={name}")
-                    .ToList();//pass sql parameter 
-
-
-            }
+            Console.WriteLine("E-Commerce Management System");
         }
     }
 }

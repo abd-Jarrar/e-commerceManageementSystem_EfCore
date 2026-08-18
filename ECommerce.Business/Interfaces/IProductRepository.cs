0fac13dfcfeaae29f335d370458222a94c63f4f1
abood.jarrar <abd.a.jarrar@gmail.com>
2026-08-18 21:21:39 +0300

    add the product interface

---
+++ ECommerce.Business/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Interfaces
+{
+    public class IProductRepository
+    {
+        Product? GetById(int id);
+
+        List<Product> GetAll();
+
+        void Add(Product product);
+
+        void Update(Product product);
+
+        void Delete(Product product);
+    }
+}
---
+++ ECommerce.Database/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Business.Entities;
+using ECommerce.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Database.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
